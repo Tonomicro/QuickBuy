0a70036afb7adbcc7c75ba00c88305c52c153029
ToNoMicro Denis <tonomicro@outlook.com>
2019-06-30 19:52:47 -0300

    inclui papagaio

--- QuickBuy.Web/Program.cs
+++ QuickBuy.Web/Program.cs
@@ -20,5 +20,7 @@ namespace QuickBuy.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        //papagaio
     }
 }
