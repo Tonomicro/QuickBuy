c3135893e6fb21a86a9a720dbff162b369a65f65
ToNoMicro Denis <tonomicro@outlook.com>
2019-06-30 19:46:46 -0300

    coloquei espaco

--- QuickBuy.Web/Program.cs
+++ QuickBuy.Web/Program.cs
@@ -20,5 +20,6 @@ namespace QuickBuy.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
