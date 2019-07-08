c28a48d81014f81b85c647076532e6a355b0f01e
ToNoMicro Denis <tonomicro@outlook.com>
2019-07-07 21:05:37 -0300

    mapeamento de entidades e contexto

--- QuickBuy.Repositorio/Contexto/QuickbuyContexto.cs
+++ QuickBuy.Repositorio/Contexto/QuickbuyContexto.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repositorio.Config;
 
 namespace QuickBuy.Repositorio.Contexto
 {
@@ -20,6 +21,17 @@ namespace QuickBuy.Repositorio.Contexto
         public QuickbuyContexto(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace QuickBuy.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            var connectionString = Configuration.GetConnectionString("NySqlConnection");
+            var connectionString = Configuration.GetConnectionString("QuickBuyDB");
             services.AddDbContext<QuickbuyContexto>(option => 
                                 option.UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
 
