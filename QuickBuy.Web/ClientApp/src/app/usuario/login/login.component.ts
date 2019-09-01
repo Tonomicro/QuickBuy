76e4d69788be1f31e4e8f8ceb27f475a74ad6652
ToNoMicro Denis <tonomicro@outlook.com>
2019-09-01 19:27:28 -0300

    componentes loguin e validacao

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core"
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
   selector: "app-login",
@@ -10,4 +11,22 @@ export class LoginComponent {
   public direito = "Direitos autorais para Mo Linda";
   public enderecoImagem = "https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/e35/s240x240/67740804_181988219490062_2585105947045385111_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&oh=a9e7d7a631b52f7ce71e3e6350c3935f&oe=5DD9FD3C&ig_cache_key=MjExNzA2Njg3ODQ5NzUyNDI1Mw%3D%3D.2";
   public titulo = "titulo da imagem";
+
+  public usuario;
+  public usuarioAutenticado: boolean;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    
+    if (this.usuario.email == 'amor' && this.usuario.senha == '123') {
+      this.usuarioAutenticado = true;
+    }
+  }
+
+  on_keypress() {
+    alert('digitando o email');
+  }
 }
